87e74c749c46ce2162a76465de397d0d326257bf
rano667 <ranjanm667@gmail.com>
2026-03-29 14:43:08 +0530

    feat: Load application startup initially not on every api call.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from pydantic import BaseModel
 
 from app.retriever import split_documents, create_vector_store
@@ -8,8 +9,35 @@ from app.rag import ask_rag
 from langchain_community.document_loaders import PyPDFLoader
 import os
 
+# ---- Global Variables ----
+vectorstore = None
+chunks = None
+generator = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global vectorstore, chunks, generator
+    
+    print("🚀 Starting up... Loading models and data")
+    
+    documents = load_documents("data")
+    print(f"Loaded docs: {len(documents)}")
+    
+    chunks = split_documents(documents)
+    print(f"Chunks created: {len(chunks)}")
+    
+    vectorstore, _ = create_vector_store(chunks)
+    print("Vector store ready")
+    
+    generator = load_llm()
+    print("LLM loaded")
+    
+    yield
+    
+    print("🛑 Shutting down")
+
 # ---- INIT ----
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 # Load everything once
 def load_documents(folder_path="data"):
@@ -21,15 +49,15 @@ def load_documents(folder_path="data"):
             all_docs.extend(loader.load())
     
     return all_docs
+
 # (later we optimize startup)
+# documents = load_documents("data")
+# print(f"Loaded documents: {len(documents)}")
+# chunks = split_documents(documents)
+# print(f"Total chunks: {len(chunks)}")
 
-documents = load_documents("data")
-print(f"Loaded documents: {len(documents)}")
-chunks = split_documents(documents)
-print(f"Total chunks: {len(chunks)}")
-
-vectorstore, embeddings = create_vector_store(chunks)
-generator = load_llm()
+# vectorstore, embeddings = create_vector_store(chunks)
+# generator = load_llm()
 
 # ---- API ----
 class QueryRequest(BaseModel):
--- ui/streamlit_app.py
+++ ui/streamlit_app.py
@@ -6,9 +6,11 @@ st.title("RAG Invoice Assistant")
 query = st.text_input("Ask something:")
 
 if st.button("Submit"):
-    res = requests.post(
-        "http://127.0.0.1:8000/ask",
-        json={"query": query}
-    )
+    with st.spinner("⏳ Processing your request... this may take up to a minute"):
+        res = requests.post(
+            "http://127.0.0.1:8000/ask",
+            json={"query": query}
+        )
     
+    st.success("✅ Done")
     st.write(res.json()["answer"])
